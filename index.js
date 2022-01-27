e5b26ed4ae37fc146d98c66d6d80d88946fe1dcd
Jari Kovalainen <itse@jarikovalainen.com>
2022-01-27 17:21:11 +0200

    joooo

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const request = require("request");
-const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 3000;
 
 
@@ -8,7 +7,7 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/HTML; charset=utf-8');
     res.write("<h1>Millainen on WEB arkkitehtuuri?</h1>"); //write a response to the client
-    res.write("<h2>Tällaiselta se näyttää</h2> ");
+    res.write("<h2>Tällaiselta se näyttää...</h2> ");
     /*res.write(
         " <img src='./web_sovellus_kuva.png'>"
     );*/
@@ -20,6 +19,4 @@ const server = http.createServer((req, res) => {
     res.end("<strong>This is the end</strong>"); //end the response
 });
 
-server.listen(PORT, hostname, () => {
-    console.log(`Server running at http://${hostname}:${PORT}/`);
-});
+server.listen(PORT);
